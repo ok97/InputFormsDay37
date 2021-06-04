d11a9880f686e5d63c01a73decd892b8b191ed2c
ok97 <omkhawshi0@gmail.com>
2021-06-04 22:08:38 +0530

    Added: Validation Phone Number

--- js/inputform.js
+++ js/inputform.js
@@ -43,3 +43,24 @@ text.addEventListener('input', function ()
             }
          });
 
+
+         /* UC3:- As a User need to follow pre-defined Mobile Format.
+                  - E.g. 91 9919819801 - Country code follow by space and 10 digit number.
+         */
+      
+        const mobileInput = document.querySelector("#tel");
+        const errorMobile = document.querySelector(".tel-error");
+        mobileInput.addEventListener('input', function ()
+          {
+            let mobileRegex = RegExp("^[0-9]{2}[ ]*[0-9]{10}$"); //Pattern
+            if (mobileRegex.test(mobileInput.value))
+            {
+                errorMobile.textContent = "";
+            }
+            else
+            {
+                errorMobile.textContent = " Invalid Mobile Number"
+            }
+          });
+
+        
